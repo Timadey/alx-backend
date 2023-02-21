c9d25171ae1732ed9599d2ee098dc81daf2013d3
Timadey <dlktimothy@gmail.com>
2023-02-21 03:37:17 +0100

    Modity 1-simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -45,6 +45,6 @@ class Server:
         assert (type(page) == int and type(page_size) == int)
         assert (page > 0 and page_size > 0)
         start_index, end_index = index_range(page, page_size)
-        if start_index >= len(self.dataset()):
+        if start_index > len(self.dataset()):
             return []
         return self.dataset()[start_index:end_index]
