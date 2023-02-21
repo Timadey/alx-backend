95998235e36df5557bfad27bf94b50eea1edff54
Timadey <dlktimothy@gmail.com>
2023-02-21 03:41:32 +0100

    Bap

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
-"""
-Alx Backend Pagination
-
-"""
-
-
+'''Pagination module
+'''
+from typing import Tuple, List
 import csv
-from typing import List, Tuple
-import math
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Function that returns a tuple of size two containing a
-    start index and an end index corresponding to the range
-    of indexes to return in a list for those particular pagination
-    parameters
-    """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+    '''Returns a range oof indexes of a pagination param
+    '''
+    first = (page - 1) * page_size
+    last = first + page_size
+    return (first, last)
 
 
 class Server:
@@ -42,9 +33,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert (type(page) == int and type(page_size) == int)
-        assert (page > 0 and page_size > 0)
-        start_index, end_index = index_range(page, page_size)
-        if start_index > len(self.dataset()):
+        '''get data based on ranges of index
+        '''
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        first, last = index_range(page, page_size)
+        data = self.dataset()
+        if first > len(data):
             return []
-        return self.dataset()[start_index:end_index]
+        return data[first:last]
