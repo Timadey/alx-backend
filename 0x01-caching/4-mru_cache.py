6cbd71dba5bf3baa63d15b804f49b0278c8fa8e0
Timadey <dlktimothy@gmail.com>
2023-02-22 22:49:16 +0100

    Use Elo's

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,89 +1,39 @@
 #!/usr/bin/env python3
-
-"""
-Caching Module
-This module contains a class BasicCache
-The class contains methods that can be used to perform basic caching operations
-
-Example:
-    >> basic = BasicCache
-    >> basic.put('A', 'first item')
-    >> print(basic.get('A'))
-    first item
-
-    The above example initiated a new BasicCache, basic.
-    Then using the put method, we added 'first item' with key 'A'
-    The item added was retrieved using the get method.
-
-Imports:
-    BaseCaching: The Base method for implementing Caching operations
-"""
-
-from functools import wraps
-
-BaseCaching = __import__("base_caching").BaseCaching
-
-
-def recent_access(method):
-    """Decorator that increment the access count of a key
-    """
-    @wraps(method)
-    def wrapper(self, *args):
-        if args[0] is None:
-            return
-        res = method(self, *args)
-        self.access_count += 1
-        self.key_count[args[0]] = self.access_count
-        return res
-    return wrapper
+"""This file contains the MRUCache class"""
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """A MRUCache class that inherits BaseCaching
-    It implements the Most Recently used caching policy
+    """Class MRUCache"""
 
-    Methods:
-        put: put a new item into the cache with the given key
-        get: get an item from the cache with the given key
-    Inherits:
-        BaseCaching
-    """
     def __init__(self):
-        """Initialize a new BaseCaching Class
-        """
+        """Initialization function"""
         super().__init__()
-        self.access_count = 0
-        self.key_count = {}
+        self.cache_monitor = {}
+        self.cache_counter = 0
 
-    @recent_access
     def put(self, key, item):
-        """Put an item into the cache with the given key
-        Args:
-            key: str : The key associated with the item to be added.
-                Can not be None
-            item: any: The item to be added
-        Return:
-            Nothing
-        """
-        if key is None or item is None:
-            return
-        cache = self.cache_data
-        cache[key] = item
+        """the put method"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            self.cache_counter += 1
+            self.cache_monitor[key] = self.cache_counter
 
-        if len(cache) > BaseCaching.MAX_ITEMS:
-            recent = self.key_count
-            mru = max(recent, key=recent.get)
-            del self.cache_data[mru]
-            del self.key_count[mru]
-            print(f"DISCARD: {mru}")
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # mru = max(self.cache_monitor, key=self.cache_monitor.get)
+                mru = list(self.cache_monitor.keys())[list(
+                       self.cache_monitor.values()).index(
+                        self.cache_counter - 1)]
 
-    @recent_access
+                del self.cache_monitor[mru]
+                del self.cache_data[mru]
+                print("DISCARD: {}".format(mru))
+
     def get(self, key):
-        """Get an item in cache linked to the key
-        Args:
-            key: str: The key that is mapped with item to be retrieved
-        Return:
-            item: any: the item retrieved with the key
-        """
-        item = self.cache_data.get(key)
-        return item
+        """the get method"""
+        if key is not None and self.cache_data.get(key):
+            self.cache_counter += 1
+            self.cache_monitor[key] = self.cache_counter
+            return self.cache_data[key]
+        else:
+            return None
